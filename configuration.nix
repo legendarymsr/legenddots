0e79fa39e04c7e65e0ffc0aa39fb8063df12416b
legendarymsr <minimunken.ah@gmail.com>
2026-05-03 22:45:15 +0200

    Fix bad package names and stub configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,5 @@
+{ pkgs, ... }:
+
+{
+  system.stateVersion = "25.11";
+}
--- home/packages.nix
+++ home/packages.nix
@@ -3,7 +3,7 @@
 {
   home.packages = with pkgs; [
     git tmux htop btop ripgrep fd fzf
-    nmap metasploit burpsuite hashcat john wireshark-qt ghidra-bin sqlmap ffuf
-    radare2 bettercap aircrack-ng empire responder android-tools
+    nmap metasploit burpsuite hashcat john wireshark-qt ghidra sqlmap ffuf
+    radare2 bettercap aircrack-ng android-tools
   ];
 }
